6a1977afd2ca195fc914a50fa4763a7f1f92827a
glegat <guillaume.legat@student.uclouvain.be>
2022-03-21 17:18:04 +0100

    adding gitignore et a hello world of main

---
+++ main.py
@@ -0,0 +1,9 @@
+def hello_world(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'{name} !')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    hello_world('Hello World')
+
