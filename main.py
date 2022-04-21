8082238fd0beb9c45b2dc67f6154ad7d77429212
glegat <guillaume.legat@student.uclouvain.be>
2022-04-21 10:52:41 +0200

    try select GPU

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import os
+# select a gpu
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
 import sys
 import random
 # import cv2
@@ -19,17 +25,17 @@ import eval
 # The main entry point for this module
 def main():
     t1 = time.perf_counter()
-    resf = open("result.txt", 'w')
-    # Train a model
-    context = sys.argv[1]
-    model_name = train.train(resf, context)
-    t2 = time.perf_counter()
-    resf.write(f"model name : {model_name}")
-    resf.write(str(f"train computation time {t2 - t1:0.4f} seconds"))
-    print("training done, launching evaluation :")
-    eval.evaluateB(resf,context, model_name)
-    print("evaluation done")
-    resf.close()
+    # resf = open("result.txt", 'w')
+    # # Train a model
+    # context = sys.argv[1]
+    # model_name = train.train(resf, context)
+    # t2 = time.perf_counter()
+    # resf.write(f"model name : {model_name}")
+    # resf.write(str(f"train computation time {t2 - t1:0.4f} seconds"))
+    # print("training done, launching evaluation :")
+    # eval.evaluateB(resf,context, model_name)
+    # print("evaluation done")
+    # resf.close()
 
 
 # Tell python to run main method
