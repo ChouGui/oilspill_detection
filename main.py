76b1052b3d1cb81985d9d59f5e683d9548a04a0b
glegat <guillaume.legat@student.uclouvain.be>
2022-05-23 13:36:58 +0200

    print errors occured in error.txt

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ warnings.filterwarnings('ignore')
 import tensorflow as tf
 import random
 import time
-
+import traceback
 import train
 import eval
 import plotRes
@@ -23,23 +23,30 @@ import plotRes
 
 # The main entry point for this module
 def main():
-    resf = open("result/result.txt", 'w')
-    os.system("rm done")
+    with open('result//error.txt', 'a') as errorf:
+        try:
+            resf = open("result/result.txt", 'w')
+            os.system("rm done")
 
-    # Train a model
-    if launch == "t" or launch == "tv":
-        t1 = time.perf_counter()
-        _, histo = train.train(resf, context, name, epochs)
-        plotRes.plotRes(histo, epochs)
-        t2 = time.perf_counter()
-        resf.write(f"Trained model name : {name}\n")
-        #print(f"train computation time {t2 - t1:0.2f} seconds")
-        resf.write(str(f"train computation time {t2 - t1:0.2f} seconds\n"))
-    if launch == "v" or launch == "tv":
-        eval.evaluateB(resf, context, name)
-        #print("evaluation done")
-    resf.close()
-    os.system("touch done")
+            # Train a model
+            if launch == "t" or launch == "tv":
+                t1 = time.perf_counter()
+                _, histo = train.train(resf, context, name, epochs)
+                plotRes.plotRes(histo, epochs)
+                t2 = time.perf_counter()
+                resf.write(f"Trained model name : {name}\n")
+                # print(f"train computation time {t2 - t1:0.2f} seconds")
+                resf.write(str(f"train computation time {t2 - t1:0.2f} seconds\n"))
+            if launch == "v" or launch == "tv":
+                eval.evaluateB(resf, context, name)
+                # print("evaluation done")
+            resf.close()
+            os.system("touch done")
+            raise ValueError("Some error occurred")
+        except Exception as e:
+            e_type, e_val, e_tb = sys.exc_info()
+            traceback.print_exception(e_type, e_val, e_tb, file=errorf)
+
 
 
 # Tell python to run main method
