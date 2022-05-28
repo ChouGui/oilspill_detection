01c49b76ee06923f2ca2fd4bb6a43b4995dd0532
glegat <guillaume.legat@student.uclouvain.be>
2022-05-28 18:10:46 +0200

    retire total de valid car plantage dans open d image

--- meantrain.py
+++ meantrain.py
@@ -8,6 +8,8 @@ if context == "ping" or context == "cass":
     gpus = sys.argv[2]
     os.environ["CUDA_VISIBLE_DEVICES"] = gpus
 
+import warnings
+warnings.filterwarnings('ignore')
 import pandas as pd
 import tensorflow as tf
 
@@ -21,23 +23,22 @@ def run(xs, context="cass", epochs=10, batch_size=16, lr=0.0001):
     # Create generators for training and validation
     # Making real time data augmentation
     # v6, v9, r50, r101, d121 = [], [], [], [], []
-    valmeans = [[], [], [], [], []]
+    #valmeans = [[], [], [], [], []]
     accmeans = [[], [], [], [], []]
     a = 0
+    tgen = utils.get_generator(str(train_path), batch_size)
+    #vgen = utils.get_generator(str(test2_path), batch_size)
     for x in xs:  # XS IS LR
-
-        tgen = utils.get_generator(str(train_path), batch_size)
-        vgen = utils.get_generator(str(test2_path), batch_size)
-
+        batch_size = x
         for m in range(len(params.models)):
             accmean = 0
-            valmean = 0
+            #valmean = 0
             for i in range(params.nbrruns):
                 print(f"({a}/{totruns}) : {x} - {params.models[m]} - [{i}/{params.nbrruns}]")
                 a += 1
                 model = utils.cust_model(params.models[m])
                 # model.summary()
-                model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=x), loss='categorical_crossentropy',
+                model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr), loss='categorical_crossentropy',
                               metrics=['acc'])
                 # reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.01, patience=3, min_lr=0.001)
 
@@ -47,42 +48,43 @@ def run(xs, context="cass", epochs=10, batch_size=16, lr=0.0001):
                     class_weight={0: 0.12, 1: 0.88},
                     steps_per_epoch=tsam // batch_size,
                     batch_size=batch_size,
-                    validation_data=vgen,
-                    validation_steps=vsam // batch_size,
+                    #validation_data=vgen,
+                    #validation_steps=vsam // batch_size,
                     epochs=epochs,
                     verbose=0)  # ,
                 # callbacks=[reduce_lr])
 
                 acc = round(history.history['acc'][epochs - 1], 3)
-                valacc = round(history.history['val_acc'][epochs - 1], 3)
-                print(f"acc : {acc} - valacc : {valacc}")
-                valmean += valacc
+                #valacc = round(history.history['val_acc'][epochs - 1], 3)
+                print(f"acc : {acc}") # - valacc : {valacc}")
+                #valmean += valacc
                 accmean += acc
-            valmeans[m].append(round(valmean / params.nbrruns, 3))
+            #valmeans[m].append(round(valmean / params.nbrruns, 3))
             accmeans[m].append(round(accmean / params.nbrruns, 3))
         print(f"{x} just done")
         print("accmeans")
         print(accmeans)
-        print("valmeans")
-        print(valmeans)
+        #print("valmeans")
+        #print(valmeans)
     # convert array into dataframe
     accdf = pd.DataFrame(accmeans)
-    valdf = pd.dataFrame(valmeans)
+    #valdf = pd.dataFrame(valmeans)
     # save the dataframe as a csv file
     accdf.to_csv("mean/accmean.csv")
-    valdf.to_csv("mean/valmean.csv")
-    return accmeans, valmeans
+    #valdf.to_csv("mean/valmean.csv")
+    return accmeans #, valmeans
 
 # The main entry point for this module
 def main():
-    with open('result//error.txt', 'a') as errorf:
+    with open('mean//error.txt', 'a') as errorf:
         try:
             print(f"RUN : | NAME - {params.name} | EPOCHS - {params.epochs} | BATCH SIZE - {params.bs}"
                   f" | LEARNING RATE - {params.lr} | COMMENTS - {params.comment}")
-            accmeans,valmeans = run(params.xs, context, params.epochs, params.bs, params.lr)
+            #accmeans,valmeans = run(params.xs, context, params.epochs, params.bs, params.lr)
+            accmeans = run(params.xs, context, params.epochs, params.bs, params.lr)
             #plotmean(x,means,title = 'Evaluation of epochs',xlabel = 'Epoch')
             utils.plotmean(params.xs,accmeans,params.name+"acc","Training Accuracy", params.xlabel)
-            utils.plotmean(params.xs,valmeans,params.name+"val","Validation Accuracy", params.xlabel)
+            #utils.plotmean(params.xs,valmeans,params.name+"val","Validation Accuracy", params.xlabel)
 
             # send a mail when finish
             server = smtplib.SMTP('smtp.googlemail.com', 587)
